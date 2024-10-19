3514b5fd6a0abd7247ac88828274465f9b004509
persipica <likekhs0107@naver.com>
2024-10-19 19:27:23 +0900

    1st

--- app/page.tsx
+++ app/page.tsx
@@ -171,13 +171,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://gptonline.ai/ko/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          © teamProject. All rights reserved.
-        </a>
+        © teamProject. All rights reserved.
       </footer>
     </div>
   )
